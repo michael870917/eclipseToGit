64ff149ee6997c55ab59a327f940b2d9af174c09
michael870917 <michael870917@gmail.com>
2014-07-27 19:58:29 -0500

    proandola
    proandola

---
+++ class.php
@@ -0,0 +1,12 @@
+<?php
+
+class Armandola{
+	public function saber(){
+		echo "comando 1";
+	}
+	
+	public function saber2(){
+		echo "comando 2";
+	}
+}
+?>
---
+++ ver.php
@@ -0,0 +1,6 @@
+<?php
+include_once 'class.php';
+
+$datos = new Armandola();
+
+echo $datos->saber();
